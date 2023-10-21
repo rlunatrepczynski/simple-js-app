cefa4741128e647767a4d3307e1e9179e23cab85
rlunatrepczynski <crenee72@hotmail.com>
2023-10-20 21:41:36 -0700

    Update scripts.js
    Updated js/scripts.js to include an array for the variable pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
-let 
+let pokemonList = [
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ['water']
+  },
+  {
+    name: "Snorlax",
+    height: 2.1,
+    types: ['normal']
+  },
+  {
+    name: "Gengar",
+    height: 1.5,
+    types: ['ghost', 'poison']
+  }
+];
 
