fa7a3f97962c366d48a6bd60a1aae0b9b886cfd6
rlunatrepczynski <crenee72@hotmail.com>
2023-10-25 23:09:16 -0700

    Update scripts.js
    Added comments for loop and conditional for pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,10 @@ let pokemonList = [
   }
 ];
 
+//Created a for loop for pokemonList
 for (let i = 0; i < pokemonList.length; i++) {
   document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
   if (pokemonList[i].height > 2) {
-    document.write(' - Wow! That\'s a big one!');
+    document.write(' - Wow! That\'s a big one!');//created conditional for pokemon height
   }
 }
