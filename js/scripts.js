1e21d0faf50923a431d564d0dd745abbf9517b78
rlunatrepczynski <crenee72@hotmail.com>
2023-10-31 22:32:07 -0700

    Update scripts.js
    Added IIFE with getAll and add functions, Wrapped Pokemon list with IIFE. Exercise 1.5 IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,40 @@
+//Created IIFE with getAll and add functions
+let pokemonRepository = function () {
+  //Created an array for pokemonList
+  let pokemonList = [
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: ['water']
+    },
+    {
+      name: "Snorlax",
+      height: 2.1,
+      types: ['normal']
+    },
+    {
+      name: "Gengar",
+      height: 1.5,
+      types: ['ghost', 'poison']
+    }
+  ];
 
-//Created an array for pokemonList
-let pokemonList = [
-  {
-    name: "Squirtle",
-    height: 0.5,
-    types: ['water']
-  },
-  {
-    name: "Snorlax",
-    height: 2.1,
-    types: ['normal']
-  },
-  {
-    name: "Gengar",
-    height: 1.5,
-    types: ['ghost', 'poison']
+  function getAll() {
+    return pokemonList;
   }
-];
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
+//Retrieve Pokemon Array from IIFE
+let pokemonList = pokemonRepository.getAll();
 //Created a forEach loop for pokemonList
 pokemonList.forEach(function (pokemon) {
   if (pokemon.height > 2) {
