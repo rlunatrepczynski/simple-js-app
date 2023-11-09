e48a0323b4351089bd3a5b3fc6b8ccf0908db866
rlunatrepczynski <crenee72@hotmail.com>
2023-11-08 22:05:09 -0700

    Update scripts.js
    Removed pokemon arrary and added fetch for exercise 1.7.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,8 @@
 //Created IIFE with getAll and add functions
 let pokemonRepository = (function () {
   //Created an array for pokemonList
-  let pokemonList = [
-    {
-      name: "Squirtle",
-      height: 0.5,
-      types: ['water']
-    },
-    {
-      name: "Snorlax",
-      height: 2.1,
-      types: ['normal']
-    },
-    {
-      name: "Gengar",
-      height: 1.5,
-      types: ['ghost', 'poison']
-    }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function showDetails(pokemon) {
     console.log(pokemon);
@@ -31,7 +16,7 @@ let pokemonRepository = (function () {
     button.classList.add("button-class");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-    button.addEventListener('click', function () {
+    button.addEventListener('click', function (event) {
       showDetails(pokemon);
     });
   }
@@ -44,16 +29,58 @@ let pokemonRepository = (function () {
     pokemonList.push(pokemon);
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
   return {
-    getAll: getAll,
     add: add,
-    addListItem: addListItem
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 })();
 
 //Retrieve Pokemon Array from IIFE
 let pokemonList = pokemonRepository.getAll();
 //Created a forEach loop for pokemonList
-pokemonList.forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonList.forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
