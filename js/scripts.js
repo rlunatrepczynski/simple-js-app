ba1d593c0c0c5794917eb807f46e3674a986f120
rlunatrepczynski <crenee72@hotmail.com>
2023-11-12 21:33:13 -0700

    Update scripts.js
    Created a modal for pokemon app for exercise 1.8.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,62 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  function showDetails(pokemon) {
-    console.log(pokemon);
+  //modal container
+  let modalContainer = document.querySelector(".modal-container");
+
+  function showModal(pokemon) {
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+
+
+    let closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = "Close";
+    closeButtonElement.addEventListener("click", hideModal);
+
+    let titleElement = document.createElement("h1");
+    titleElement.innerText = 'Pokemon name' + ': ' + pokemon.name;
+
+    let contentElement = document.createElement("p");
+    contentElement.innerText = 'Pokemon height' + ': ' + pokemon.height;
+
+
+    let myImage = document.createElement('img');
+    myImage.src = pokemon.imageUrl;
+    modal.appendChild(myImage);
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add("is-visible");
   }
+
+  let dialogPromiseReject;
+
+  function hideModal() {
+    let modal = document.querySelector(".modal");
+    modal.remove();
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
@@ -36,7 +89,8 @@ let pokemonRepository = (function () {
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
-          detailsUrl: item.url
+          detailsUrl: item.url,
+          imageUrl: item.myImage,
         };
         add(pokemon);
       });
