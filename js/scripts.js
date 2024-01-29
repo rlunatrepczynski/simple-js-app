31cb43ded2d3a60ffadb4b4531677f601100aa2e
crene <crenee72@hotmail.com>
2024-01-28 22:23:30 -0700

    Update scripts.js
    increased load time

--- js/scripts.js
+++ js/scripts.js
@@ -159,5 +159,5 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   setTimeout(function () {
     loader.style.display = "none";
-  }, 5000);
+  }, 15000);
 });
