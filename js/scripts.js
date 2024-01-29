81e4bccf8908e4ea849ab6aa1714fef655eb6576
crene <crenee72@hotmail.com>
2024-01-28 22:21:03 -0700

    Update scripts.js
    Increased the wait time

--- js/scripts.js
+++ js/scripts.js
@@ -159,5 +159,5 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   setTimeout(function () {
     loader.style.display = "none";
-  }, 1500);
+  }, 5000);
 });
